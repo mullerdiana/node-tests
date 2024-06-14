c41a9eabd30b8af9130513730bd7d81ef4c7b3b7
Diana Müller <diana.muller@globalhitss.com.br>
2024-06-14 11:15:09 -0300

    teste de asserções

---
+++ assert.js
@@ -0,0 +1,11 @@
+/* eslint-disable max-len */
+// Tipo específico de alidação, checagem ou verificação. As asserções poderão ser verdadeiras ou falsas.
+if (2 === 2) // true
+{
+  if (15 > 42) // false
+  {
+    if (2 >= 2) // true
+
+    { ['abacaxi', 'banana'].includes('banana'); }
+  }
+} // true
