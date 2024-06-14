9e57cd6161c96fc917def4a297c6aa391a742ce8
Diana Müller <diana.muller@globalhitss.com.br>
2024-06-14 11:26:12 -0300

    assert

--- assert.js
+++ assert.js
@@ -1,11 +1,14 @@
+/* eslint-disable no-self-compare */
 /* eslint-disable max-len */
 // Tipo específico de alidação, checagem ou verificação. As asserções poderão ser verdadeiras ou falsas.
-if (2 === 2) // true
-{
-  if (15 > 42) // false
-  {
-    if (2 >= 2) // true
+if (2 === 2) {
+  // true
+  if (15 > 42) {
+    // false
+    if (2 >= 2) {
+      // true
 
-    { ['abacaxi', 'banana'].includes('banana'); }
+      ['abacaxi', 'banana'].includes('banana');
+    }
   }
 } // true
